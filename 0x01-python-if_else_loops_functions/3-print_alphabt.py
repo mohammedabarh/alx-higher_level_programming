017a2abefe67e0310d2312ac228de2c8479f4555
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 14:08:21 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if char != 101 and char != 113:
-        print(chr(char), end='')
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
