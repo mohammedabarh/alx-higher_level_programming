0fad020f345b08a25282d933b6b467b91844cd7f
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 15:00:17 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-numbers = range(100)  # Create a range from 0 to 99
-formatted_numbers = [f'{num:02d}' for num in numbers]  # Format each number with leading zeros
-print(', '.join(formatted_numbers))  # Join the formatted numbers with a comma and print
+for num in range(100):
+    if num < 99:
+        print('{:02d}, '.format(num), end='')
+    else:
+        print('{:02d}'.format(num))
+
