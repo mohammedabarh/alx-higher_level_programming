edf16062866c048a6b5bd6d6931bc0188fa9a726
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 15:18:53 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num < 99:
+num = 0
+while num < 100:
+    if num != 99:
         print('{:02d}, '.format(num), end='')
     else:
         print('{:02d}'.format(num))
+    num += 1
 
