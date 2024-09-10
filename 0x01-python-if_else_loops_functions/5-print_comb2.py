038ddbdfc7e2f9795a612e16e610aa1b6b2c13ad
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 14:55:47 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(f"{i:02}", end=", " if i < 99 else "\n")
+numbers = range(100)  # Create a range from 0 to 99
+formatted_numbers = [f'{num:02d}' for num in numbers]  # Format each number with leading zeros
+print(', '.join(formatted_numbers))  # Join the formatted numbers with a comma and print
