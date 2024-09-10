babdde073676d57fdee54bc6f79c96d818557060
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 13:51:19 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
-
+#!/usr/bin/env python3
+for i in range(97, 123):
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end="")
