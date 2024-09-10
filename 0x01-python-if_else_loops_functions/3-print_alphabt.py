be2b7724967f1e142ba82c4a8b01c1b51115fc68
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 13:37:10 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:  # Exclude 'e' (101) and 'q' (113)
-        print(chr(i), end='')
+
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ('q', 'e'):
+        print(chr(char), end="")
+
