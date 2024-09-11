096938fb3cc4b1dc743fdd2bdcca56513a003961
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 18:11:59 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def add(a, b):
-    return (a + b)
 
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+    
+    Args:
+    a (int): The first integer.
+    b (int): The second integer.
+    
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
