15b70030758ce0879bd750ff4d7c8016ee46bd7e
mohammedabarh <abmohammed606@gmail.com>
2024-09-10 18:38:48 -0700

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+    
+    Args:
+    a (int/float): The base number.
+    b (int): The exponent.
+    
+    Returns:
+    int/float: The result of a raised to the power of b.
+    """
+    return a ** b
