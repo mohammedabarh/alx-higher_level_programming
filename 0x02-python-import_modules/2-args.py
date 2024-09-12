888605292d22b5d8d0f1c56c50bc054fa26ca7f2
mohammedabarh <abmohammed606@gmail.com>
2024-09-12 06:16:35 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
+    import sys
+
     argc = len(sys.argv) - 1
-    
+    argv = sys.argv[1:]
+
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
     else:
         print(f"{argc} arguments:")
-    
-    for i, arg in enumerate(sys.argv[1:], start=1):
+
+    for i, arg in enumerate(argv, start=1):
         print(f"{i}: {arg}")
