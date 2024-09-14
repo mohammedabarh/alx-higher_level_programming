953c6fac4fafa121c5b02af16411a4e32d47e757
mohammedabarh <abmohammed606@gmail.com>
2024-09-14 10:03:15 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure each tuple has at least 2 elements, using 0 for missing values
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
+    len_a, len_b = len(tuple_a), len(tuple_b)
     
-    # Sum the first two elements of each tuple
-    return (a[0] + b[0], a[1] + b[1])
+    new_tuple = (
+        (tuple_a[0] if len_a >= 1 else 0) + (tuple_b[0] if len_b >= 1 else 0),
+        (tuple_a[1] if len_a >= 2 else 0) + (tuple_b[1] if len_b >= 2 else 0)
+    )
+    
+    return new_tuple
