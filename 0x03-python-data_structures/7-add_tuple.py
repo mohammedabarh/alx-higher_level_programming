d69167ad184836b3c57f1dac0f13cd0e9a03d95a
mohammedabarh <abmohammed606@gmail.com>
2024-09-14 10:23:24 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extend tuples to at least 2 elements, filling with zeros
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    
-    # Add the first two elements of each tuple
-    return (a[0] + b[0], a[1] + b[1])
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
