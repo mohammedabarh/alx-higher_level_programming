599091a84f279efb7fb3fb76ea3fab9f34baecbf
mohammedabarh <abmohammed606@gmail.com>
2024-09-14 08:25:14 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for number in reversed(my_list):
-            print("{:d}".format(number))
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
