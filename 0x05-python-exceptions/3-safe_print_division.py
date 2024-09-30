2afebb5e46ff55182cf89f2e4ac181e5b81bda58
mohammedabarh <abmohammed606@gmail.com>
2024-09-30 09:45:58 -0700

    python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+def safe_print_division(a, b):
     try:
-        for i in range(x):
-            if isinstance(my_list[i], int):  # Check if the element is an integer
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-    except IndexError:
-        pass  # Ignore the exception when x is greater than list length
-    print()  # Print a new line after printing the integers
-    return count
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
