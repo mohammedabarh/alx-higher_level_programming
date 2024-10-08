5f56b364ecb185f6ac11cf2f058cb5133ccf71db
mohammedabarh <abmohammed606@gmail.com>
2024-10-08 14:18:56 -0700

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-# Import the LockedClass from the specified module
-LockedClass = __import__('101-locked_class').LockedClass
+"""Defines a locked class."""
 
-# Create an instance of LockedClass
-instance = LockedClass()
-# Set the first_name attribute
-instance.first_name = "John"
 
-# Attempt to set a last_name attribute, which should raise an exception
-try:
-    instance.last_name = "Snow"
-except Exception as error:
-    print("[{}] {}".format(error.__class__.__name__, error))
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
