bda84f9a9ee892c301ecfb1627178492be918eaf
mohammedabarh <abmohammed606@gmail.com>
2024-10-09 04:43:55 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-This module contains a function that adds two integers.
-"""
+"""This module contains a function that adds two integers."""
+
 
 def add_integer(a, b=98):
     """
-    Adds two integers.
+    Add two integers.
 
     Args:
         a (int or float): The first number.
@@ -21,5 +20,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
