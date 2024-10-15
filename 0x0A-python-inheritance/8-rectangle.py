028a7638eb74cb77a68b4b3bb40b10ae831ba7ea
mohammedabarh <abmohammed606@gmail.com>
2024-10-14 19:01:04 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
+"""8-rectangle.py
+Defines a Rectangle class that inherits from BaseGeometry.
+"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle, inheriting from BaseGeometry.
-    """
+    """Represents a rectangle, inheriting from BaseGeometry."""
 
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance.
+        """Initializes a Rectangle instance.
 
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle.
+
+        Returns:
+            str: Informal string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
