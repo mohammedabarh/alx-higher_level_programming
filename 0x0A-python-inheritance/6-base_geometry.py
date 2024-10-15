047ca644eaffcbb4da2fdcec78b8bc627645e420
mohammedabarh <abmohammed606@gmail.com>
2024-10-14 17:46:16 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Module for geometric operations."""
+"""6-base_geometry.py
+Defines a BaseGeometry class.
+"""
+
 
 class BaseGeometry:
-    """A class to represent basic geometric shapes."""
+    """A class representing basic geometric operations."""
 
     def area(self):
-        """Calculate the area of the shape.
+        """Calculates the area of the geometry.
 
         Raises:
-            Exception: If the area method is not implemented in a derived class.
+            Exception: If the area method is not implemented.
         """
         raise Exception('area() is not implemented')
