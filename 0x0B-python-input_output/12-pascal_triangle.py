669035915217845cb836c70101fd6e220bbe65ec
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 20:36:06 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,23 +3,17 @@
 
 
 class Student:
-    """Represents a student with a first name, last name, and age."""
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a JSON representation of the Student instance.
-
-        If attrs is provided, only the attributes specified in attrs
-        will be included in the returned dictionary.
-        """
         if attrs is None:
             return self.__dict__
-
         new_dictionary = {}
         for key, value in self.__dict__.items():
             if key in attrs:
