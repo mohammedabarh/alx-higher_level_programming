40ef6a860d864d1bdbe269838fa55f701bec39a2
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 20:10:30 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+"""Module that defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
@@ -18,14 +18,11 @@ def pascal_triangle(n):
 
     triangle = [[1]]
 
-    for _ in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j-1] + prev_row[j])
-
-        new_row.append(1)
-        triangle.append(new_row)
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
