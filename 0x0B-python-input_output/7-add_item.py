912982106355ef94d0698e3a97aaf4629070ecb3
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 19:34:17 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a file."""
-import sys
-from os import path
+
+import json
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-    filename = "add_item.json"
-    
-    # Load existing items or create an empty list if file doesn't exist
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-    
-    # Add command-line arguments to the list
-    my_list.extend(sys.argv[1:])
-    
-    # Save the updated list to the JSON file
-    save_to_json_file(my_list, filename)
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file, using a JSON"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
