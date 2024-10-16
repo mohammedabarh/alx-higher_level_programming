d99fa546f27807ea5b0fd68a30722dc83ca0bc40
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 20:14:19 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function to generate Pascal's Triangle."""
 
+
 def pascal_triangle(n):
     """Generate Pascal's Triangle of size n.
 
@@ -15,16 +16,16 @@ def pascal_triangle(n):
         return []
 
     triangles = [[1]]  # Start with the first row of the triangle.
-    
-    while len(triangles) != n:
+
+    while len(triangles) < n:
         tri = triangles[-1]  # Get the last row of the triangle.
         tmp = [1]  # Initialize the new row with the first element '1'.
-        
+
         # Calculate the values for the new row.
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
-        
+
         tmp.append(1)  # End the new row with the last element '1'.
         triangles.append(tmp)  # Append the new row to the triangle.
-    
+
     return triangles
