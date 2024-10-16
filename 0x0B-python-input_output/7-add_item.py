df28ad1f295678de855e069661b57c7325686393
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 19:19:21 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Module that loads, adds and saves arguments to a Python list"""
-from sys import argv
+"""Script that adds all arguments to a Python list and saves them to a file."""
+import sys
+from os import path
 
 
-load_file = __import__('6-load_from_json_file').load_from_json_file
-save_file = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    json_list = load_file('add_item.json')
-except (ValueError, FileNotFoundError):
-    json_list = []
-
-for item in argv[1:]:
-    json_list.append(item)
-
-save_file(json_list, 'add_item.json')
+    filename = "add_item.json"
+    
+    # Load existing items or create an empty list if file doesn't exist
+    if path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+    
+    # Add command-line arguments to the list
+    my_list.extend(sys.argv[1:])
+    
+    # Save the updated list to the JSON file
+    save_to_json_file(my_list, filename)
