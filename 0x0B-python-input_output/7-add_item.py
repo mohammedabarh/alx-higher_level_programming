a44840c04376249d2c322dbe3bf7974bdfd87c95
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 18:33:16 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+"""7-add_item.py
+Adds all command line arguments to a list and saves them to a JSON file.
+"""
 
-import json
+import sys
+from load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Saves an object to a text file, using a JSON representation."""
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+filename = 'add_item.json'
 
+# Load existing items from the file, or initialize an empty list if the file doesn't exist.
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+# Add command line arguments (excluding the script name) to the list.
+items.extend(sys.argv[1:])
+
+# Save the updated list back to the JSON file.
+save_to_json_file(items, filename)
