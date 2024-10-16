40f6cd72f6f04766987037bcdef64ac53ed116c5
mohammedabarh <abmohammed606@gmail.com>
2024-10-15 18:31:20 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-import sys
-from os import path
 
-# Importing the functions from previous tasks
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
 
-filename = "add_item.json"
-
-# Load existing list if file exists, otherwise start with an empty list
-if path.exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-
-# Add new command-line arguments (excluding the script name)
-items.extend(sys.argv[1:])
-
-# Save the updated list back to the JSON file
-save_to_json_file(items, filename)
+def save_to_json_file(my_obj, filename):
+    """Saves an object to a text file, using a JSON representation."""
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
 
