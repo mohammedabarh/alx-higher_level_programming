be84092d7f0c3301ce3413d8c4087e4235b7440e
mohammedabarh <abmohammed606@gmail.com>
2024-11-20 06:12:15 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists privileges of MySQL users
+-- Lists privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Lists privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
