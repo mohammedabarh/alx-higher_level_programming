c8c038ed76eb48ade9197d89db07f0e9189dd5a5
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 15:08:53 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
+    
     for row in rows:
-        print(row)
+        print(row)  # No need for the additional check; the query already filters
+
     cur.close()
     db.close()
