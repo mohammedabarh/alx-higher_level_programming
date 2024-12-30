5923ec488fcc24f8ba426a1bf5e641dc99e275d1
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 15:29:12 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the states table"""
+"""Script that takes in an argument and displays all values in the states table."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
+    
+    # Query to select states based on the provided name
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
 
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
-
     for row in rows:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
 
     cur.close()
     db.close()
