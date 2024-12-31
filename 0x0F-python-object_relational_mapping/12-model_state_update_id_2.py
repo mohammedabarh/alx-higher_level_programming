48d492ed82ec7431e0624a000bc701a27af3d6df
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 19:00:20 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,15 +8,15 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Create a new database engine
+    # Create a new database engine using provided credentials
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    # Query the State object with id = 2
+    # Query the State object with ID = 2
     state = session.query(State).filter_by(id=2).first()
     if state:
-        # Change the state name to "New Mexico"
+        # Update the state name to "New Mexico"
         state.name = "New Mexico"
         session.commit()  # Commit the changes to the database
