8ae076f91ebefbf95bb82710a190c8f1f05c4b4b
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 16:09:00 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the states table"""
+"""
+Script that takes in an argument and displays all values in the states table
+of a database where name matches the argument
+"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+    # Connect to MySQL database using command line arguments
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create cursor object to execute queries
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' \
-                ORDER BY id ASC".format(sys.argv[4]))
+
+    # Execute SELECT query with user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cur.execute(query.format(sys.argv[4]))
+
+    # Fetch and print matching rows
     rows = cur.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:
             print(row)
+
+    # Close database connection
     cur.close()
     db.close()
