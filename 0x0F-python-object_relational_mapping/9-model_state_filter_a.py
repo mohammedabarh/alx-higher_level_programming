3ab61fc656a2447578fcbfed897843fa31ee7032
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 17:25:36 -0800

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """City class for database table cities"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
