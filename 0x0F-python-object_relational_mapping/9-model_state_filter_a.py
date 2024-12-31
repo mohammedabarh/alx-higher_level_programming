9840a62bde22e8056e4237b8c40053d3c0c40db0
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 18:40:53 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,8 +8,11 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
