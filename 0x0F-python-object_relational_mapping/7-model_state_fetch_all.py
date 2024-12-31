531438cf81f8659c1bf5fc3b5a6271b487d0aed1
mohammedabarh <abmohammed606@gmail.com>
2024-12-30 17:43:12 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+Script that lists all State objects from the database hbtn_0e_6_usa
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-Base = declarative_base()
 
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                          format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                          pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-class State(Base):
-    """State class"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
