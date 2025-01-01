9c93d79f6afbfe9ae5bbb4b5d0074490d2814bbe
mohammedabarh <abmohammed606@gmail.com>
2025-01-01 08:45:43 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,7 +3,6 @@
 This script removes all State objects from the database
 whose names contain the letter 'a'.
 It utilizes the SQLAlchemy module for database interaction.
-
 """
 
 from model_state import Base, State
