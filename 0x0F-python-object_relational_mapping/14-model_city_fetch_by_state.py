97481dd0761f2836b823686c143f34fd2c12a93b
mohammedabarh <abmohammed606@gmail.com>
2025-01-01 13:17:20 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database"""
-import sys
+"""
+Fetches and displays all City objects from the database along with their states.
+"""
+
+from model_city import City
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Create a database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id):
+    # Configure a session class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session instance
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    # Query to join State and City, ordered by city ID
+    city = session.query(State, City).join(City).order_by(City.id)
+    for state, city in city:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Close the session to free resources
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Contains the class definition of a City"""
+"""
+Defines the City class, representing a city in the database.
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from model_state import Base, State
+from model_state import Base
+
 
 class City(Base):
-    """City class"""
-    __tablename__ = 'cities'
-    
+    """
+    Represents a city and its attributes.
+
+    Attributes:
+        id (int): Unique identifier for the city.
+        name (str): Name of the city.
+        state_id (int): Identifier for the associated state.
+    """
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
