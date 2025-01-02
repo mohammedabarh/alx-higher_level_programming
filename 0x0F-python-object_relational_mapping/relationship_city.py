ed4f3ec8a186cbabf091b2cd998c23bbecb88419
mohammedabarh <abmohammed606@gmail.com>
2025-01-01 16:43:57 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text, ForeignKey
+"""Contains the class definition of a City"""
+from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
-"""
-    Contains the class definition of a City.
-"""
-
 
 class City(Base):
-    """
-        The ``City`` class.
-    """
+    """City class"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text
-from sqlalchemy.orm import relationship
+"""Contains State class and Base instance"""
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-"""
-    Contains State class and Base instance.
-"""
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
 class State(Base):
-    """
-        The ``States`` class.
-    """
+    """State class"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
