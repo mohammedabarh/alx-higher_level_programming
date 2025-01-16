d72c6ea086499a79f979232c2f7e790bd95082ce
mohammedabarh <abmohammed606@gmail.com>
2025-01-16 11:37:44 -0800

    0x10-python-network_0

---
+++ 0x10-python-network_0/6-main.py
@@ -0,0 +1,5 @@
+print(find_peak([1, 2, 4, 6, 3]))  # Output: 6
+print(find_peak([4, 2, 1, 2, 3, 1]))  # Output: 4 or 3
+print(find_peak([2, 2, 2]))  # Output: 2
+print(find_peak([]))  # Output: None
+print(find_peak([-2, -4, 2, 1]))  # Output: 2
