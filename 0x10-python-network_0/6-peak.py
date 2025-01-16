30ab23461fb0d4b7be425312274881363f40c765
mohammedabarh <abmohammed606@gmail.com>
2025-01-16 11:53:18 -0800

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Find a peak in a list of unsorted integers."""
-
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers."""
+    """
+    Find a peak in a list of unsorted integers
+    Complexity: O(log(n))
+    """
     if not list_of_integers:
         return None
-    low, high = 0, len(list_of_integers) - 1
-    while low < high:
-        mid = (low + high) // 2
+    
+    # Binary search approach
+    left, right = 0, len(list_of_integers) - 1
+    
+    while left < right:
+        mid = (left + right) // 2
+        
+        # If mid element is smaller than its next element, peak is on the right side
         if list_of_integers[mid] < list_of_integers[mid + 1]:
-            low = mid + 1
+            left = mid + 1
+        # Otherwise, peak is on the left side or at mid
         else:
-            high = mid
-    return list_of_integers[low]
+            right = mid
+    
+    return list_of_integers[left]
