80ec8c50995bb50b1787d4e99afe41138f7027ad
mohammedabarh <abmohammed606@gmail.com>
2025-01-16 11:40:21 -0800

    0x10-python-network_0

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,5 +1,10 @@
-print(find_peak([1, 2, 4, 6, 3]))  # Output: 6
-print(find_peak([4, 2, 1, 2, 3, 1]))  # Output: 4 or 3
-print(find_peak([2, 2, 2]))  # Output: 2
-print(find_peak([]))  # Output: None
-print(find_peak([-2, -4, 2, 1]))  # Output: 2
+#!/usr/bin/python3
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
