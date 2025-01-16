092e57be038d0868954c9983f05faca106eb247d
mohammedabarh <abmohammed606@gmail.com>
2025-01-16 11:27:59 -0800

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to a specific endpoint and captures the response
-curl -s -X PUT -L -d "user_id=98" "0.0.0.0:5000/catch_me"
+# This script makes a request to a specific URL to get a certain response.
+curl -s -X PUT "0.0.0.0:5000/catch_me" -H "Origin:School"
