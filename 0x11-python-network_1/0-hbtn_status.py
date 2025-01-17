4181b8bd0f3bf7a4488dfc7e58731fac3ab56410
mohammedabarh <abmohammed606@gmail.com>
2025-01-17 10:35:44 -0800

    0x11-python-network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 import urllib.request
 
-def fetch_status():
-    try:
-        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-            html = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(html)))
-            print("\t- content: {}".format(html))
-            print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except Exception as e:
-        print("An error occurred: ", e)
-
-if __name__ == "__main__":
-    fetch_status()
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
