88cf5ccf0ee61abc7de0e7f488819522258214f9
mohammedabarh <abmohammed606@gmail.com>
2025-01-17 11:01:59 -0800

    0x11-python-network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {body.decode('utf-8')}")
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
