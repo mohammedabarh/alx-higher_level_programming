e0982bf951e41abf6aa196d9000c28a89cea64a8
mohammedabarh <abmohammed606@gmail.com>
2025-01-17 18:27:57 -0800

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Script to send a POST request to a specified URL with a letter as a parameter.
-
-This script sends a letter to the server at http://0.0.0.0:5000/search_user.
-If no letter is provided, it sends an empty string.
+"""Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
 """
+
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    # Set the letter to an empty string if no argument is provided
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
 
-    # Send a POST request to the server
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    payload = {'q': letter}
+
+    request = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+
     try:
-        # Attempt to parse the response as JSON
-        response = r.json()
+        response = request.json()
         if response == {}:
-            print("No result")
+            print('No result')
         else:
-            # Print the ID and name from the JSON response
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
     except ValueError:
-        # Handle cases where the response is not valid JSON
-        print("Not a valid JSON")
+        print('Not a valid JSON')
